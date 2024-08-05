36ce2e7ed54e0ce655968c83f9b461091c0e290d
Siyuan <146351547+Frosty7463@users.noreply.github.com>
2024-08-05 17:51:46 +0100

    Update element.py
    changed the polarizability values to the recommended ones, changed the atomic radius of all elements for training and predicitng to Van der Waals atomic radius

--- element.py
+++ element.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
-
 class Element_prediction:
     """
     A class used to represent a model for predicting polarizability based on atomic radius
@@ -16,6 +15,10 @@ class Element_prediction:
         Each entry contains atomic radius and polarizability values.
 
     Methods:
+    _prepare data():
+        Prepares the data by converting it into pandas DataFrames and separating features and targets.
+    cross_validation():
+        Splits the data into training and testing sets for each group and period.
     train_models():
         Trains linear regression models for each group and period in the data.
     evaluate_models():
@@ -143,18 +146,18 @@ class Element_prediction:
 
     def print_predictions(self):
         """
-        Prints the predicted polarizability for predefined atomic radii for groups and periods.
+        Prints the predicted polarizability for Van der Waals atomic radii for groups and periods.
         """
+
+        # The Van der Waals atomic radius (in pm) for elements to predict 
         elements_to_predict = {
-            'group1': {'Rb': 220, 'Cs': 244, 'Fr': 260}, # Covalent atomic radius
-            'group2': {'Sr': 195, 'Ba': 215, 'Ra': 221}, # Covalent atomic radius
-            'group17': {'I': 139, 'At': 150}, # Covalent atomic radius
-            'noble': {'Xe': 140, 'Rn': 150}, # Covalent atomic radius
-            'period4': {'Sc': 211, 'Ti': 187, 'V': 179, 'Cr': 189, 'Mn': 197, 'Fe': 194, 
-                        'Co': 192, 'Ni': 163, 'Ga': 187, 'Ge': 211, 'As': 185, 'Se': 190}
-             # Van der Waals atomic radius
+            'group1': {'Rb': 303, 'Cs': 343, 'Fr': 348}, 
+            'group2': {'Sr': 249, 'Ba': 268, 'Ra': 283}, 
+            'group17': {'I':198,'At': 202}, 
+            'noble': {'Xe': 216, 'Rn': 220}, 
+            'period4': {'Cr': 189, 'Mn': 197, 'Fe': 194, 'Co': 192, 'Ni': 163, 'Cu':140,
+                        'Zn':139,'Ga': 187, 'Ge': 211, 'As': 185, 'Se': 190, 'Br': 183, 'Kr':202}  
         }  
-         
         for group, elements in elements_to_predict.items():
             print(f"\nPredictions for {group}:")
             for element, radius in elements.items():
@@ -164,39 +167,31 @@ class Element_prediction:
 # Data input
 data = {
     'group1': {
-        'Atomic_Radius': [31, 131, 160, 201], # Covalent atomic radius for H, Li, Na, K
-        'Polarizability': [0.667, 24.330, 24.110, 43.400]
+        'Atomic_Radius': [120, 182, 227, 275], # Van der Waals atomic radius for H, Li, Na, K (in pm)
+        'Polarizability': [4.51, 164, 163, 290]
     },
     'group2': {
-        'Atomic_Radius': [96, 126, 176], # Covalent atomic radius for Be, Mg, Ca
-        'Polarizability': [5.600, 10.600, 22.800]
+        'Atomic_Radius': [153, 173, 231], # Van der Waals atomic radius for Be, Mg, Ca (in pm)
+        'Polarizability': [37.7, 71.2, 161]
     },
     'group17': {
-        'Atomic_Radius': [57, 102, 120], # Covalent atomic radius for F, Cl, Br
-        'Polarizability': [0.557, 2.180, 3.050]
+        'Atomic_Radius': [135, 175, 183], # Van der Waals atomic radius for F, Cl, Br (in pm)
+        'Polarizability': [3.74, 13.6, 20]
     },
     'noble_gas': {
-        'Atomic_Radius': [28, 58, 106, 116], # Covalent atomic radius for He, Ne, Ar, Kr
-        'Polarizability': [0.208, 0.381, 1.664, 2.498]
+        'Atomic_Radius': [140, 154, 188, 202], # Van der Waals atomic radius for He, Ne, Ar, Kr (in pm)
+        'Polarizability': [1.38, 2.66, 11.1, 16.8]
     },
     'period4': {
-        'Atomic_Radius': [275, 231, 139, 183, 202], # Van der Waals atomic radius for K, Ca, Zn, Br, Kr
-        'Polarizability': [43.400, 22.800, 5.75, 3.05, 2.498]
+        'Atomic_Radius': [275, 231, 211, 187, 179], # Van der Waals atomic radius for K, Ca, Sc, Ti, V (in pm)
+        'Polarizability': [290, 161, 97, 100, 87]
     }
+
 }
 
 if __name__ == "__main__":
-   # Creating an instance of PeriodicTableModel
    model = Element_prediction(data)
-
-   # Training the models
    model.train_models()
-
-   # Evaluating the models
    model.evaluate_models()
-
-   # Printing evaluation results
    model.print_evaluation()
-
-   # Printing predictions
    model.print_predictions()
